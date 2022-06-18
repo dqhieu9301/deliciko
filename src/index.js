d2c75330e51e2d6f9239c333ad5eacf9bd8b5626
your name <dinhhieu9301@gmail.com>
2022-06-18 18:54:02 +0700

    comeback router

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
