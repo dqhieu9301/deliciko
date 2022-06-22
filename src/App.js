422a2daca471055ac50199120c68333b57169e53
dqhieu9301 <90315708+dqhieu9301@users.noreply.github.com>
2022-06-22 14:38:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <HashRouter basename='/deliciko'>
+    <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
@@ -40,7 +40,7 @@ function App() {
         draggable
         pauseOnHover
       />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
